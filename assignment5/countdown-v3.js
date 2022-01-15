353c90b8218ee5c912e9fd1b3ec783e02a9af580
pujaraanil <97371380+pujaraanil@users.noreply.github.com>
2022-01-15 03:48:41 -0800

    Updated assignments.

--- assignment5/countdown-v3.js
+++ assignment5/countdown-v3.js
@@ -10,21 +10,21 @@ function Countdown() {
     
     // setTimeout Library Function to Display Countdown Timer
     setTimeout(() => { 
-        
+        // Once countdown timer reaches 0, display Blastoff!
         if(countdowntimer==0) { 
             // Edit innerHTML for CountdownDisplay to 0 then display Blastoff after
-            document.getElementById("CountdownWarning").innerHTML = "Blasting Off...";
-            document.getElementById("CountdownDisplay").innerHTML = countdowntimer;
+            document.getElementById("CountdownWarning").innerHTML = "Blasting Off..."; // Additional notice at 0
+            document.getElementById("CountdownDisplay").innerHTML = countdowntimer; // Show 0
             setTimeout(() => {
                 // Display Blastoff after 1 second if Timer is at Countdown is at 0
-                document.getElementById("CountdownDisplay").innerHTML = "Blastoff!";
-                document.getElementById("CountdownWarning").innerHTML = "";
+                document.getElementById("CountdownDisplay").innerHTML = "Blastoff!"; // Display Blastoff! after countdown ended
+                document.getElementById("CountdownWarning").innerHTML = ""; // Remove message in additional notice area.
                 },1000)
         }
         else { 
             // If Countdown is not equal to 0, display current counter in the ID CountdownDisplay
             document.getElementById("CountdownDisplay").innerHTML = countdowntimer;
-            if(countdowntimer<=5) { 
+            if(countdowntimer<=5) {  // Display additional notice message if countdown is at 5 seconds or less!
                 document.getElementById("CountdownWarning").innerHTML = "Warning Less than 1/2 way to launch, time left = " + countdowntimer;
             }
         }
